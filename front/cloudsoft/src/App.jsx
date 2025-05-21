ba6cb7cee3588e8e1dfb8d811049915baa1b26c8
Bagharia <hu.alexis@hotmail.com>
2025-05-21 13:33:47 +0200

    ajout de la page analyze

--- front/cloudsoft/src/App.jsx
+++ front/cloudsoft/src/App.jsx
@@ -1,9 +1,11 @@
 import Home from "./components/Home";
+import Analyse from "./components/Analyze";
 
 function App() {
   return (
-    <div className="min-h-screen bg-cover bg-center bg-no-repeat bg-black" style={{ }}>
-      <Home/>
+    <div className="min-h-screen bg-cover bg-center bg-no-repeat bg-gray-800" style={{ }}>
+      {/* <Home/> */}
+      {<Analyse/>}
     </div>
   );
 }
---
+++ front/cloudsoft/src/components/Analyze.jsx
@@ -0,0 +1,16 @@
+import { useState } from "react";
+
+const Analyse = () => {
+    return (
+        <div>
+            <h1 className="font-krona text-3xl text-white flex justify-center">ARMATUS</h1>
+            <div className="p-6 bg-black rounded-xl flex flex-col gap-4 max-w-lg mx-auto my-auto mt-28">
+                <div >
+
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Analyse;
